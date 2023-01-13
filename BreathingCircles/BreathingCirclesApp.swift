57d73249dd9652c1ce61cb9f9a6b2838c827c66c
Ronald de Souza <ronald.souza.stg@gmail.com>
2023-01-12 21:43:24 -0300

    initial commit

---
+++ BreathingCircles/BreathingCirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathingCirclesApp.swift
+//  BreathingCircles
+//
+//  Created by Test on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BreathingCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
